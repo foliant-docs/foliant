1cbd1b5518b695d7efd0ce5ad6603126ff871f0d
Konsantin Molchanov <moigagoo@live.com>
2017-01-19 17:47:07 +0400

    Git utils: Add get repo function.

--- foliant/gitutils.py
+++ foliant/gitutils.py
@@ -3,6 +3,7 @@ version.
 """
 
 import subprocess
+from os.path import join
 
 def get_version():
     """Generate document version based on git tag and number of revisions."""
@@ -28,3 +29,28 @@ def get_version():
         pass
 
     return '.'.join(components) if components else None
+
+
+def get_repo(repo_url, target_dir):
+    """Clone git repository if it's not cloned yet or pull changes otherwise.
+    """
+
+    repo_name = repo_url.split('/')[-1].split('.')[0]
+    repo_path = join(target_dir, repo_name)
+
+    print("Fetching %s..." % repo_url, end=' ')
+
+    if subprocess.run(
+        "git clone %s %s" % (repo_url, repo_path),
+        stderr=subprocess.PIPE
+    ):
+        subprocess.run(
+            "git --work-tree %s pull origin master" % repo_path,
+            stderr=subprocess.PIPE
+        )
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    get_repo("git@git.restr.im:docs-itv/doorstopper_itv.git", "foliantcache")
