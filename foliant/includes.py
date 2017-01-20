9ad45e46cba41a773faa01480624b119568a8cf0
Konsantin Molchanov <moigagoo@live.com>
2017-01-20 17:13:46 +0400

    Includes: Update syntax for options.

--- foliant/includes.py
+++ foliant/includes.py
@@ -3,7 +3,9 @@ import re
 
 def process_includes(content):
     include_statement = re.compile(
-        r"\{\{\s*(<(?P<repo>.+)\>)?(?P<path>.+?)(\#(?P<from_heading>.+?)(:(?P<to_heading>.+?))?)?\s*(\|\s*(?P<options>.+))?\s*\}\}"
+        r"\{\{\s*(<(?P<repo>.+)\>)?" +
+        r"(?P<path>.+?)(\#(?P<from_heading>.+?)(:(?P<to_heading>.+?))?)?" +
+        r"\s*(\|\s*(?P<options>.+))?\s*\}\}"
     )
 
     for include in re.finditer(include_statement, content):
@@ -13,10 +15,11 @@ def process_includes(content):
         print("From heading: %s" % include.group("from_heading"))
         print("To heading: %s" % include.group("to_heading"))
 
-        options = str(include.group("options"))
+        options = include.group("options")
 
-        for option in (option.strip() for option in options.split(",")):
-            print("Option: %s" % option)
+        if options:
+            for option in options.split(","):
+                print("Option: %s" % option.strip())
 
     return content
 
@@ -24,7 +27,7 @@ def process_includes(content):
 if __name__ == "__main__":
     process_includes("""This is a sample that includes an include statement:
 
-{{ <myrepo>/path/to/file.md#heading1:heading2 | nohead, sethead(3) }}
+{{ <myrepo>/path/to/file.md#heading1:heading2 | nohead, sethead:3 }}
 
 Another one:
 
