c4d7888a215fc27afa96dd3e0c26a4b1f2d3f8d5
Konsantin Molchanov <moigagoo@live.com>
2017-01-25 17:47:56 +0400

    Includes: Add image adjustment.

--- foliant/includes.py
+++ foliant/includes.py
@@ -1,5 +1,5 @@
 import re
-import os.path
+import os.path as ospa
 
 
 def convert_value(value):
@@ -83,11 +83,56 @@ def process_headings(content, from_heading, to_heading=None,
     return result
 
 
-def process_local_include(path, from_heading=None, to_heading=None,
-                          options={}):
-    current_dir = os.path.dirname(os.path.abspath(__file__))
+def find_image(image_path, lookup_dir):
+    def normabspath(path):
+        return ospa.normcase(ospa.abspath(path))
 
-    with open(os.path.join(current_dir, path), encoding="utf8") as incl_file:
+    if ospa.isfile(ospa.join(lookup_dir, image_path)):
+        return ospa.join(lookup_dir, image_path)
+
+    else:
+        level = 0
+        current_lookup_dir = lookup_dir
+        next_lookup_dir = ospa.join(lookup_dir, "../" * level, "images")
+
+        while normabspath(current_lookup_dir) != normabspath(next_lookup_dir):
+            if ospa.isfile(ospa.join(next_lookup_dir, image_path)):
+                return ospa.join(next_lookup_dir, image_path)
+
+            current_lookup_dir = next_lookup_dir
+            level += 1
+            next_lookup_dir = ospa.join(lookup_dir, "../" * level, "images")
+
+    return ''
+
+
+def process_images(content, lookup_dir):
+    def normalize_slashes(path):
+        return '/'.join(path.split(ospa.sep))
+
+    def sub(image):
+        image_caption = image.group("caption")
+        image_path = image.group("path")
+
+        if ospa.isfile(ospa.join(lookup_dir, image_path)):
+            adjusted_image_path = ospa.join(lookup_dir, image_path)
+
+        else:
+            adjusted_image_path = find_image(image_path, lookup_dir)
+
+        return "![%s](%s)" % (
+            image_caption,
+            normalize_slashes(adjusted_image_path)
+        )
+
+    image_pattern = re.compile(r"\!\[(?P<caption>.*)\]\((?P<path>.+)\)")
+
+    return image_pattern.sub(sub, content)
+
+
+def process_local_include(path, from_heading=None, to_heading=None,
+                          options={}, sources_dir="."):
+    with open(ospa.join(sources_dir, path), encoding="utf8") as incl_file:
         incl_content = incl_file.read()
 
         if from_heading:
@@ -98,39 +143,50 @@ def process_local_include(path, from_heading=None, to_heading=None,
                 options
             )
 
+        incl_content = process_images(
+            incl_content,
+            ospa.split(ospa.join(sources_dir, path))[0]
+        )
+
     return incl_content
 
 
-def process_remote_include(repo, path, from_heading, to_heading, options={}):
+def process_remote_include(repo, path, from_heading, to_heading, options={},
+                           sources_dir="."):
     return "Remote"
 
 
-def expand_include(include):
+def expand_include(include, sources_dir):
     if include.group("repo"):
         return process_remote_include(
             include.group("repo"),
             include.group("path"),
             include.group("from_heading"),
             include.group("to_heading"),
-            extract_options(include.group("options"))
+            extract_options(include.group("options")),
+            sources_dir
         )
     else:
         return process_local_include(
             include.group("path"),
             include.group("from_heading"),
             include.group("to_heading"),
-            extract_options(include.group("options"))
+            extract_options(include.group("options")),
+            sources_dir
         )
 
 
-def process_includes(content):
+def process_includes(content, sources_dir="."):
+    def sub(include):
+        return expand_include(include, sources_dir)
+
     include_pattern = re.compile(
         r"\{\{\s*(<(?P<repo>.+)\>)?" +
         r"(?P<path>.+?)(\#(?P<from_heading>.+?)(:(?P<to_heading>.+?))?)?" +
         r"\s*(\|\s*(?P<options>.+))?\s*\}\}"
     )
 
-    return include_pattern.sub(expand_include, content)
+    return include_pattern.sub(sub, content)
 
 
 if __name__ == "__main__":
@@ -140,7 +196,7 @@ if __name__ == "__main__":
 
 Another one:
 
-{{../test-project/sources/chapter1.md#Cras scelerisque tincidunt bibendum | sethead:2 }}
+{{test-project/sources/chapter1.md }}
 
 Here's some text after it.
 """
