9fa3d29ff16d3dda1ac63e8d53d8cdf743e7486c
Konstantin Molchanov <moigagoo@live.com>
2017-01-31 23:47:12 +0400

    Fix formatting.

--- foliant/__init__.py
+++ foliant/__init__.py
@@ -5,7 +5,8 @@ as Google Documents.
 """
 
 __name__ = "foliant"
-__description__ = "Documentation generator that builds PDF, Docx, and TeX from a single Markdown source."
+__description__ = "Documentation generator that builds PDF, Docx, and TeX from\
+    a single Markdown source."
 __author__ = "Konstantin Molchanov"
 __author_email__ = "moigagoo@live.com"
-__version__  = "0.2.7"
+__version__ = "0.2.7"
