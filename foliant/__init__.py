b876cb05aafd75c94ef9daf15488e6d11f84bd6d
Konstantin Molchanov <moigagoo@live.com>
2017-08-30 22:15:58 +0400

    Bump version to 0.4.4.

--- foliant/__init__.py
+++ foliant/__init__.py
@@ -9,4 +9,4 @@ __description__ = "Documentation generator that builds PDF, Docx, and TeX from\
     a single Markdown source."
 __author__ = "Konstantin Molchanov"
 __author_email__ = "moigagoo@live.com"
-__version__ = "0.4.3"
+__version__ = "0.4.4"
