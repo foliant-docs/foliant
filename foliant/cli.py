255d73c8f9dacc5baa37fefc07228658bee9ad8a
Konstantin Molchanov <moigagoo@live.com>
2017-09-04 17:58:29 +0400

    Remove Swagger and Apidoc converters.

--- foliant/cli.py
+++ foliant/cli.py
@@ -3,10 +3,6 @@
 Usage:
   foliant (build | make) <target> [--path=<project-path>]
   foliant (upload | up) <document> [--secret=<client_secret*.json>]
-  foliant (swagger2markdown | s2m) <swagger-location> [--output=<output-file>]
-    [--template=<jinja2-template>] [--additional=<swagger-location>]
-  foliant (apidoc2markdown | a2m) <apidoc-location> [--output=<output-file>]
-    [--template=<jinja2-template>]
   foliant (-h | --help)
   foliant --version
 
@@ -15,19 +11,13 @@ Options:
   -v --version                       Show version.
   -p --path=<project-path>           Path to your project [default: .].
   -s --secret=<client_secret*.json>  Path to Google app's client secret file.
-  -o --output=<output-file>          Path to the converted Markdown file
-                                     [default: api.md]
-  -t --template=<jinja2-template>    Custom Jinja2 template for the Markdown
-                                     output.
-  -a --additional=<swagger-location> Complementary Swagger file to fill in
-                                     missing values from the main one.
 """
 
 from docopt import docopt
 import colorama
 from colorama import Fore
 
-from foliant import builder, uploader, swagger2markdown, apidoc2markdown
+from foliant import builder, uploader
 from foliant import __version__ as foliant_version
 
 
@@ -43,28 +33,6 @@ def main():
 
     elif args["upload"] or args["up"]:
         result = uploader.upload(args["<document>"])
-
-    elif args["swagger2markdown"] or args["s2m"]:
-        swagger_location = args["<swagger-location>"]
-        output_file = args["--output"]
-        template_file = args.get("--template")
-        additional_swagger_location = args.get("--additional")
-        swagger2markdown.convert(
-            swagger_location,
-            output_file,
-            template_file,
-            additional_swagger_location
-        )
-
-        result = output_file
-
-    elif args["apidoc2markdown"] or args["a2m"]:
-        apidoc_location = args["<apidoc-location>"]
-        result = args["--output"]
-        template_file = args.get("--template")
-        apidoc2markdown.convert(apidoc_location, output_file, template_file)
-
-        result = output_file
 
     print("---")
     print(Fore.GREEN + "Result: %s" % result)
