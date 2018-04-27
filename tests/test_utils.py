13b0d7c578b008b97c2f0c64ce3c0de6d3e095df
Konstantin Molchanov <moigagoo@live.com>
2018-04-27 23:13:29 +0400

    Tests: Utils: Add config parsers test.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,16 @@
+import foliant
 from foliant import utils
 
-
 def test_get_available_tags():
+    '''Check that no tags are registered in vanilla Foliant.'''
+
     assert utils.get_available_tags() == set()
+
+
+def test_get_available_config_parsers():
+    '''Check that there are exactly two builtin config parsers: path and include.'''
+    
+    parsers = utils.get_available_config_parsers()
+    assert set(parsers.keys()) == {'path', 'include'}
+    assert issubclass(parsers['path'], foliant.config.base.BaseParser)
 
