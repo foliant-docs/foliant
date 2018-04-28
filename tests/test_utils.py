6ed4f90f82d8edc5cb0f2d0fa846f9b0162e8793
Konstantin Molchanov <moigagoo@live.com>
2018-04-28 15:12:31 +0400

    Tests: Utils: Add CLIs test.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,8 +9,18 @@ def test_get_available_tags():
 
 def test_get_available_config_parsers():
     '''Check that there are exactly two builtin config parsers: path and include.'''
-    
+
     parsers = utils.get_available_config_parsers()
+
     assert set(parsers.keys()) == {'path', 'include'}
     assert issubclass(parsers['path'], foliant.config.base.BaseParser)
 
+def test_get_available_clis():
+    '''Check that there's exactly one default CLI with a single ``make`` command.'''
+
+    clis = utils.get_available_clis()
+
+    assert set(clis.keys()) == {'make'}
+
+    for cli_type in clis.values():
+        assert issubclass(cli_type, foliant.cli.base.BaseCli)
