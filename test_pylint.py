d03f98cc7fc1aff0ff1ada1ceb1f903e3b845d09
Konstantin Molchanov <moigagoo@live.com>
2018-05-27 00:10:47 +0400

    Fxi PyLint test.

--- test_pylint.py
+++ test_pylint.py
@@ -8,7 +8,8 @@ PYLINT_SCORE_THRESHOLD = 8.50
 def test_pylint(capfd):
     run('pylint foliant', shell=True)
     score_pattern = re.compile(r'Your code has been rated at (?P<score>\d\.\d{2})/10.*')
-    score_line = capfd.readouterr().out.splitlines()[-4]
+
+    score_line = capfd.readouterr().out.strip().splitlines()[-1]
     score = float(score_pattern.match(score_line).group('score'))
 
     assert score >= PYLINT_SCORE_THRESHOLD
