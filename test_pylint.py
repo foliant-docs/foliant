2f79eebaa35911bc3df6cb33e1cde77f669819ee
Konstantin Molchanov <moigagoo@live.com>
2018-05-26 23:54:51 +0400

    Add PyLint test.

---
+++ test_pylint.py
@@ -0,0 +1,14 @@
+import re
+from subprocess import run
+
+
+PYLINT_SCORE_THRESHOLD = 8.50
+
+
+def test_pylint(capfd):
+    run('pylint foliant', shell=True)
+    score_pattern = re.compile(r'Your code has been rated at (?P<score>\d\.\d{2})/10.*')
+    score_line = capfd.readouterr().out.splitlines()[-4]
+    score = float(score_pattern.match(score_line).group('score'))
+
+    assert score >= PYLINT_SCORE_THRESHOLD
