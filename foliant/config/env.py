3f1911ab24503bb6f80659aa3cc4092783a6b277
Artemy Lomov <lomov@restream.rt.ru>
2020-07-20 23:14:07 +0300

    Support the `!env` YAML tag to use environment variables in the project config.

---
+++ foliant/config/env.py
@@ -0,0 +1,16 @@
+from os import getenv
+from yaml import load, add_constructor, Loader
+
+from foliant.config.base import BaseParser
+
+
+class Parser(BaseParser):
+    def _resolve_env_tag(self, _, node) -> str:
+        '''Replace value after ``!env`` with the value of referenced environment variable.'''
+
+        return load(getenv(node.value, default='null'), Loader)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        add_constructor('!env', self._resolve_env_tag)
